7e095cd62a407711c2e7a407199a9911227f38c4
Mike <82643885@qq.com>
2017-02-06 14:29:29 +0800

    ver 1.0

--- LFLiveKit/coder/LFHardwareVideoEncoder.m
+++ LFLiveKit/coder/LFHardwareVideoEncoder.m
@@ -32,8 +32,8 @@
         NSLog(@"USE LFHardwareVideoEncoder");
         _configuration = configuration;
         [self resetCompressionSession];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willEnterBackground:) name:UIApplicationWillResignActiveNotification object:nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willEnterForeground:) name:UIApplicationDidBecomeActiveNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(disEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+        //        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willEnterForeground:) name:UIApplicationDidBecomeActiveNotification object:nil];
 #ifdef DEBUG
         enabledWriteVideoFile = NO;
         [self initForFilePath];
@@ -44,32 +44,33 @@
 }
 
 - (void)resetCompressionSession {
-    if (compressionSession) {
-        VTCompressionSessionCompleteFrames(compressionSession, kCMTimeInvalid);
-
-        VTCompressionSessionInvalidate(compressionSession);
-        CFRelease(compressionSession);
-        compressionSession = NULL;
+    @synchronized (self) {
+        if (compressionSession) {
+            VTCompressionSessionCompleteFrames(compressionSession, kCMTimeInvalid);
+            
+            VTCompressionSessionInvalidate(compressionSession);
+            CFRelease(compressionSession);
+            compressionSession = NULL;
+        }
+        
+        OSStatus status = VTCompressionSessionCreate(NULL, _configuration.videoSize.width, _configuration.videoSize.height, kCMVideoCodecType_H264, NULL, NULL, NULL, VideoCompressonOutputCallback, (__bridge void *)self, &compressionSession);
+        if (status != noErr) {
+            return;
+        }
+        
+        _currentVideoBitRate = _configuration.videoBitRate;
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_MaxKeyFrameInterval, (__bridge CFTypeRef)@(_configuration.videoMaxKeyframeInterval));
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_MaxKeyFrameIntervalDuration, (__bridge CFTypeRef)@(_configuration.videoMaxKeyframeInterval/_configuration.videoFrameRate));
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_ExpectedFrameRate, (__bridge CFTypeRef)@(_configuration.videoFrameRate));
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_AverageBitRate, (__bridge CFTypeRef)@(_configuration.videoBitRate));
+        NSArray *limit = @[@(_configuration.videoBitRate * 1.5/8), @(1)];
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_DataRateLimits, (__bridge CFArrayRef)limit);
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_RealTime, kCFBooleanTrue);
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_ProfileLevel, kVTProfileLevel_H264_Main_AutoLevel);
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_AllowFrameReordering, kCFBooleanTrue);
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_H264EntropyMode, kVTH264EntropyMode_CABAC);
+        VTCompressionSessionPrepareToEncodeFrames(compressionSession);
     }
-
-    OSStatus status = VTCompressionSessionCreate(NULL, _configuration.videoSize.width, _configuration.videoSize.height, kCMVideoCodecType_H264, NULL, NULL, NULL, VideoCompressonOutputCallback, (__bridge void *)self, &compressionSession);
-    if (status != noErr) {
-        return;
-    }
-
-    _currentVideoBitRate = _configuration.videoBitRate;
-    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_MaxKeyFrameInterval, (__bridge CFTypeRef)@(_configuration.videoMaxKeyframeInterval));
-    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_MaxKeyFrameIntervalDuration, (__bridge CFTypeRef)@(_configuration.videoMaxKeyframeInterval/_configuration.videoFrameRate));
-    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_ExpectedFrameRate, (__bridge CFTypeRef)@(_configuration.videoFrameRate));
-    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_AverageBitRate, (__bridge CFTypeRef)@(_configuration.videoBitRate));
-    NSArray *limit = @[@(_configuration.videoBitRate * 1.5/8), @(1)];
-    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_DataRateLimits, (__bridge CFArrayRef)limit);
-    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_RealTime, kCFBooleanTrue);
-    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_ProfileLevel, kVTProfileLevel_H264_Main_AutoLevel);
-    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_AllowFrameReordering, kCFBooleanTrue);
-    VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_H264EntropyMode, kVTH264EntropyMode_CABAC);
-    VTCompressionSessionPrepareToEncodeFrames(compressionSession);
-
 }
 
 - (void)setVideoBitRate:(NSInteger)videoBitRate {
@@ -87,7 +88,7 @@
 - (void)dealloc {
     if (compressionSession != NULL) {
         VTCompressionSessionCompleteFrames(compressionSession, kCMTimeInvalid);
-
+        
         VTCompressionSessionInvalidate(compressionSession);
         CFRelease(compressionSession);
         compressionSession = NULL;
@@ -102,13 +103,13 @@
     CMTime presentationTimeStamp = CMTimeMake(frameCount, (int32_t)_configuration.videoFrameRate);
     VTEncodeInfoFlags flags;
     CMTime duration = CMTimeMake(1, (int32_t)_configuration.videoFrameRate);
-
+    
     NSDictionary *properties = nil;
     if (frameCount % (int32_t)_configuration.videoMaxKeyframeInterval == 0) {
         properties = @{(__bridge NSString *)kVTEncodeFrameOptionKey_ForceKeyFrame: @YES};
     }
     NSNumber *timeNumber = @(timeStamp);
-
+    
     OSStatus status = VTCompressionSessionEncodeFrame(compressionSession, pixelBuffer, presentationTimeStamp, duration, (__bridge CFDictionaryRef)properties, (__bridge_retained void *)timeNumber, &flags);
     if(status != noErr){
         [self resetCompressionSession];
@@ -124,8 +125,8 @@
 }
 
 #pragma mark -- Notification
-- (void)willEnterBackground:(NSNotification*)notification{
-    _isBackGround = YES;
+- (void)disEnterBackground:(NSNotification*)notification{
+    [self resetCompressionSession];
 }
 
 - (void)willEnterForeground:(NSNotification*)notification{
@@ -140,18 +141,18 @@ static void VideoCompressonOutputCallback(void *VTref, void *VTFrameRef, OSStatu
     if (!array) return;
     CFDictionaryRef dic = (CFDictionaryRef)CFArrayGetValueAtIndex(array, 0);
     if (!dic) return;
-
+    
     BOOL keyframe = !CFDictionaryContainsKey(dic, kCMSampleAttachmentKey_NotSync);
     uint64_t timeStamp = [((__bridge_transfer NSNumber *)VTFrameRef) longLongValue];
-
+    
     LFHardwareVideoEncoder *videoEncoder = (__bridge LFHardwareVideoEncoder *)VTref;
     if (status != noErr) {
         return;
     }
-
+    
     if (keyframe && !videoEncoder->sps) {
         CMFormatDescriptionRef format = CMSampleBufferGetFormatDescription(sampleBuffer);
-
+        
         size_t sparameterSetSize, sparameterSetCount;
         const uint8_t *sparameterSet;
         OSStatus statusCode = CMVideoFormatDescriptionGetH264ParameterSetAtIndex(format, 0, &sparameterSet, &sparameterSetSize, &sparameterSetCount, 0);
@@ -162,7 +163,7 @@ static void VideoCompressonOutputCallback(void *VTref, void *VTFrameRef, OSStatu
             if (statusCode == noErr) {
                 videoEncoder->sps = [NSData dataWithBytes:sparameterSet length:sparameterSetSize];
                 videoEncoder->pps = [NSData dataWithBytes:pparameterSet length:pparameterSetSize];
-
+                
                 if (videoEncoder->enabledWriteVideoFile) {
                     NSMutableData *data = [[NSMutableData alloc] init];
                     uint8_t header[] = {0x00, 0x00, 0x00, 0x01};
@@ -172,12 +173,12 @@ static void VideoCompressonOutputCallback(void *VTref, void *VTFrameRef, OSStatu
                     [data appendData:videoEncoder->pps];
                     fwrite(data.bytes, 1, data.length, videoEncoder->fp);
                 }
-
+                
             }
         }
     }
-
-
+    
+    
     CMBlockBufferRef dataBuffer = CMSampleBufferGetDataBuffer(sampleBuffer);
     size_t length, totalLength;
     char *dataPointer;
@@ -189,20 +190,20 @@ static void VideoCompressonOutputCallback(void *VTref, void *VTFrameRef, OSStatu
             // Read the NAL unit length
             uint32_t NALUnitLength = 0;
             memcpy(&NALUnitLength, dataPointer + bufferOffset, AVCCHeaderLength);
-
+            
             NALUnitLength = CFSwapInt32BigToHost(NALUnitLength);
-
+            
             LFVideoFrame *videoFrame = [LFVideoFrame new];
             videoFrame.timestamp = timeStamp;
             videoFrame.data = [[NSData alloc] initWithBytes:(dataPointer + bufferOffset + AVCCHeaderLength) length:NALUnitLength];
             videoFrame.isKeyFrame = keyframe;
             videoFrame.sps = videoEncoder->sps;
             videoFrame.pps = videoEncoder->pps;
-
+            
             if (videoEncoder.h264Delegate && [videoEncoder.h264Delegate respondsToSelector:@selector(videoEncoder:videoFrame:)]) {
                 [videoEncoder.h264Delegate videoEncoder:videoEncoder videoFrame:videoFrame];
             }
-
+            
             if (videoEncoder->enabledWriteVideoFile) {
                 NSMutableData *data = [[NSMutableData alloc] init];
                 if (keyframe) {
@@ -213,15 +214,15 @@ static void VideoCompressonOutputCallback(void *VTref, void *VTFrameRef, OSStatu
                     [data appendBytes:header length:3];
                 }
                 [data appendData:videoFrame.data];
-
+                
                 fwrite(data.bytes, 1, data.length, videoEncoder->fp);
             }
-
-
+            
+            
             bufferOffset += AVCCHeaderLength + NALUnitLength;
-
+            
         }
-
+        
     }
 }
 
